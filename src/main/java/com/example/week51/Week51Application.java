b59e6de2ec0231f1047877ae83c159008ba78d79
Hotelcombiend <63070141@it.kmitl.ac.th>
2022-09-20 09:47:08 +0700

    Initial commit

---
+++ src/main/java/com/example/week51/Week51Application.java
@@ -0,0 +1,13 @@
+package com.example.week51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week51Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week51/Week51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
